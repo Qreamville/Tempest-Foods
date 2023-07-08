8cc271622c4d496e7d000b7336350b2a88799daa
Qreamville <Oyeleyebalikis@yahoo.com>
2023-07-08 18:01:17 +0100

    feat: update

--- components/Modal/Checkout.tsx
+++ components/Modal/Checkout.tsx
@@ -1,13 +1,22 @@
 import { formatPrice } from "@/actions/formatPrice";
 import useCart from "@/hooks/useCart";
 import React from "react";
+import { useRouter } from "next/navigation";
+import useCartModal from "@/hooks/useCartModal";
 
 const Checkout = () => {
+  const router = useRouter();
   const cart = useCart((state) => state.cart);
+  const closeModal = useCartModal((state) => state.onClose);
   const totalPrice = cart.reduce(
     (acc, product) => acc + product.price * (product.quantity as number),
     0
   );
+
+  const checkout = () => {
+    router.push("/checkout");
+    closeModal();
+  };
 
   return (
     <div className="checkout">
@@ -15,7 +24,9 @@ const Checkout = () => {
         <span>Total ({cart.length}meals)</span>
         <span>NGN {formatPrice(totalPrice)}</span>
       </div>
-      <button className="checkout-btn">Checkout</button>
+      <button className="checkout-btn" onClick={checkout}>
+        Checkout
+      </button>
     </div>
   );
 };
